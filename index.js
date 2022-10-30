141776cf7d60ea52f49a31f3b52b6ec230394f14
Stefan Caras <stefan.caras@gmail.com>
2022-10-30 20:14:33 +0200

    update

---
+++ index.js
@@ -0,0 +1,34 @@
+let items;
+
+const createTable = () => {
+	const table = document.querySelector('.table');
+	items.forEach(item => {
+		let row = table.insertRow(0);
+		let cell1 = row.insertCell(0);
+		let cell2 = row.insertCell(1);
+		let cell3 = row.insertCell(2);
+		let cell4 = row.insertCell(3);
+		cell1.innerHTML = `<img class="cartImg" src='${item.imgURL}' />`;
+		cell2.innerHTML = `<p class=""><b>${item.name}</b></p>
+		<p class="">${item.ingredients}</p>`;
+		cell3.textContent = `$${item.price}`;
+		cell4.innerHTML = `<a class="greenBtn marginLeft" href="details.html?id=${item.id}">
+		<i class="fa fa-info-circle marginRight"></i>Details</a>`
+	});
+};
+
+const showBooks = async () => {
+	scroll(0,0);
+	// Show loader
+	document.querySelector('.table').style.display = 'none';
+	document.querySelector('.loader').style.display = 'block';
+	// Show items
+	const result = await fetch(`./index.json`);
+	items = await result.json();
+	createTable();
+	// Hide loader
+	document.querySelector('.loader').style.display = 'none';
+	document.querySelector('.table').style.display = 'block';
+};
+
+window.addEventListener('DOMContentLoaded', showBooks);
